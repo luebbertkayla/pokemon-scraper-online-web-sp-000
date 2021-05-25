1a1bcb4d293debec4e67524233e8ac1edc497eb9
Kayla Luebbert <luebbertkayla@gmail.com>
2021-05-25 19:30:33 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,4 +11,12 @@ class Pokemon
     @@all << self 
   end 
   
-  
+    def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type) VALUES (?, ?);
+    SQL
+    
+    db.execute(sql, [name, type])
+    
+  end
+
